5cc81ea3ba14cf3159c993be7d54402c7b765beb
IanJames2 <ianjms81@gmail.com>
2021-11-01 12:46:20 -0500

    Added Validations to Create Forms.

--- FirebaseMVC/Models/ProductOrService.cs
+++ FirebaseMVC/Models/ProductOrService.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
 
 namespace BusinessVenture.Models
 {
     public class ProductOrService
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Select the business that will sell this product!")]
         public int BusinessId { get; set; }
+
+        [Required(ErrorMessage = "Type in a name for your product or service to continue!")]
         public string NameOfProductOrService { get; set; }
+
+        [Range(1, 2000000000)]
+        [Required(ErrorMessage = "You must have a cost for your product or service!")]
         public int Cost { get; set; }
+
         public Business Business { get; set; }
     }
 }
